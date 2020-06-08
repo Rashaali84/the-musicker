8235f5122bf2337194b3f20430b07d5f223d0182
Rasha ali <rashaali84@gmail.com>
2020-06-09 00:51:50 +0200

    style

--- client/app/init.js
+++ client/app/init.js
@@ -2,8 +2,7 @@ fetch('/api')
   .then(res => res.json())
   .then(data => {
     console.log(data)
-    document.getElementById('root')
-      .innerHTML = data.message;
+
   })
   .catch(err => console.error(err));
 
