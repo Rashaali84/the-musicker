59bc1a07a92f28e202fa8bcf75ff1a71db0600cb
Rasha ali <rashaali84@gmail.com>
2020-06-09 19:42:44 +0200

    adding data validation

--- client/app/playlists/create-playlist.js
+++ client/app/playlists/create-playlist.js
@@ -5,21 +5,34 @@ window.onload = () => {
 function creatPlayList() {
 
     let playListName = document.getElementById('playListName').value;
-    fetch('/api/playlists/', {
-        method: 'POST',
-        body: JSON.stringify({ name: playListName }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8'
-        }
-    })
-        .then(res => {
-            if (!res.ok) {
-                throw res;
+
+    if (isEmpty(playListName) || isBlank(playListName)) {
+        alert('Please Write a play list name .. ');
+    }
+    else {
+        fetch('/api/playlists/', {
+            method: 'POST',
+            body: JSON.stringify({ name: playListName }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8'
             }
-            return res.json();
         })
-        .then(records => {
-            alert(`PlayList ${playListName} is added successfully ! `)
-        })
-        .catch(err => console.error(err));
+            .then(res => {
+                if (!res.ok) {
+                    throw res;
+                }
+                return res.json();
+            })
+            .then(records => {
+                alert(`PlayList ${playListName} is added successfully ! `)
+            })
+            .catch(err => console.error(err));
+    }
+}
+
+function isEmpty(str) {
+    return (!str || 0 === str.length);
+}
+function isBlank(str) {
+    return (!str || /^\s*$/.test(str));
 }
